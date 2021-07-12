a8c7eee47736b935108ef7deb6a66f0465a0532f
Stephan Protsack <stephan@speedandfunction.com>
2021-07-12 19:56:29 +0300

    Fix topics

--- example/create/main.go
+++ example/create/main.go
@@ -12,8 +12,8 @@ const url = "http://localhost:8088/ksql"
 
 func main() {
 	queries := []string{
-		"CREATE STREAM pages(identifier INTEGER, name STRING, event STRUCT<uuid VARCHAR, type VARCHAR, date VARCHAR>, version STRUCT<identifier INTEGER>, date_modified VARCHAR, url VARCHAR, is_part_of STRUCT<identifier VARCHAR>, article_body ARRAY<STRUCT<text VARCHAR, encoding_format VARCHAR>>) WITH (kafka_topic='aws.pages.v1', value_format='json');",
-		"CREATE STREAM versions(identifier INTEGER, name STRING, event STRUCT<uuid VARCHAR, type VARCHAR, date VARCHAR>, comment VARCHAR) WITH (kafka_topic='aws.versions.v1', value_format='json');",
+		"CREATE STREAM pages(identifier INTEGER, name STRING, event STRUCT<uuid VARCHAR, type VARCHAR, date VARCHAR>, version STRUCT<identifier INTEGER>, date_modified VARCHAR, url VARCHAR, is_part_of STRUCT<identifier VARCHAR>, article_body ARRAY<STRUCT<text VARCHAR, encoding_format VARCHAR>>) WITH (kafka_topic='loc.pages.v1', value_format='json');",
+		"CREATE STREAM versions(identifier INTEGER, name STRING, event STRUCT<uuid VARCHAR, type VARCHAR, date VARCHAR>, comment VARCHAR) WITH (kafka_topic='loc.versions.v1', value_format='json');",
 		"CREATE STREAM pages_versions AS SELECT pages.name as name, pages.identifier as identifier, versions.identifier as version__identifier, versions.comment as version__comment FROM pages LEFT JOIN versions WITHIN 10 SECONDS ON pages.version->identifier = versions.identifier;",
 	}
 
